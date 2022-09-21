f13bfbe88a771f2e5f5fc6114553d09b2c755333
Lieuwe Hummel <lieuwe@familyhummel.net>
2022-09-21 23:27:38 +0200

    Process: formatting, display error when dir is invalid

--- process-keep-json.php
+++ process-keep-json.php
@@ -14,51 +14,53 @@ use KeepToObsidian\Starred;
 require 'vendor/autoload.php';
 
 if (!isset($argv) || !is_array($argv) || count($argv) < 2) {
-	exit('No file path given');
+    exit('No file path given');
 }
 
 // $argv[0] is the name of the script
 $sourcePath = $argv[1];
-
+if (!is_dir($sourcePath)) {
+    exit('Not a directory: ' . $sourcePath);
+}
 $sourcePath = FileHelpers::trailingSlashIt($sourcePath);
 $mocList = new MocList();
 $starred = new Starred();
 
-if (is_dir($sourcePath)) {
-	FileHelpers::assertDirectoryExists($sourcePath . 'md/');
-	FileHelpers::assertDirectoryExists($sourcePath . 'md/.obsidian');
-	FileHelpers::assertDirectoryExists($sourcePath . 'md/' . KeepJSONMarkdownConverter::ARCHIVE_DIR);
-	FileHelpers::assertDirectoryExists($sourcePath . 'md/' . KeepJSONMarkdownConverter::ATTACHMENT_DIR);
-	$files = glob($sourcePath . "*.json");
-	foreach ($files as $file) {
-		$note = file_get_contents($file);
-		$note = json_decode($note, false, 512, JSON_THROW_ON_ERROR);
+FileHelpers::assertDirectoryExists($sourcePath . 'md/');
+FileHelpers::assertDirectoryExists($sourcePath . 'md/.obsidian');
+FileHelpers::assertDirectoryExists($sourcePath . 'md/' . KeepJSONMarkdownConverter::ARCHIVE_DIR);
+FileHelpers::assertDirectoryExists($sourcePath . 'md/' . KeepJSONMarkdownConverter::ATTACHMENT_DIR);
 
-		if ($note instanceof stdClass) {
-			try {
-				$converter = new KeepJSONMarkdownConverter($note);
-				if ($converter->isTrashed) {
-					continue;
-				}
-				file_put_contents($sourcePath . 'md/' . $converter->filename, $converter->document);
-				foreach ($converter->getFilesToCopy($sourcePath) as [$srcFilename, $dstFilename]) {
-					copy(
-						$sourcePath . $srcFilename,
-						$sourcePath . 'md/' . $dstFilename
-					);
-				}
-				if ($converter->isPinned) {
-					$starred->addNote($converter);
-				}
-				$mocList->addNote($converter);
-			} catch (Exception $e) {
-				echo $e->getMessage();
-				echo 'File: ' . $file;
-			}
-		}
-	}
+$files = glob($sourcePath . "*.json");
+foreach ($files as $file) {
+    $note = file_get_contents($file);
+    $note = json_decode($note, false, 512, JSON_THROW_ON_ERROR);
 
-	$starred->write($sourcePath . 'md/');
-	$mocList->writeAll($sourcePath . 'md/');
+    if ($note instanceof stdClass) {
+        try {
+            $converter = new KeepJSONMarkdownConverter($note);
+            if ($converter->isTrashed) {
+                continue;
+            }
+            file_put_contents($sourcePath . 'md/' . $converter->filename, $converter->document);
+            foreach ($converter->getFilesToCopy($sourcePath) as [$srcFilename, $dstFilename]) {
+                copy(
+                    $sourcePath . $srcFilename,
+                    $sourcePath . 'md/' . $dstFilename
+                );
+            }
+            if ($converter->isPinned) {
+                $starred->addNote($converter);
+            }
+            $mocList->addNote($converter);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            echo 'File: ' . $file;
+        }
+    }
 }
+
+$starred->write($sourcePath . 'md/');
+$mocList->writeAll($sourcePath . 'md/');
+
 exit('All done');
