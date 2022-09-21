04876493f5cacd7d0f1e824bb90c373761492a28
Lieuwe Hummel <lieuwe@familyhummel.net>
2022-09-21 23:25:44 +0200

    Formatting

--- src/KeepJSONMarkdownConverter.php
+++ src/KeepJSONMarkdownConverter.php
@@ -20,10 +20,9 @@ use stdClass;
  * Takes a Google Keep note in .json form and makes a
  * ABGEO\MDGenerator\Document ($this->document) that can be used as a string.
  */
-class KeepJSONMarkdownConverter
-{
-	public const ARCHIVE_DIR = 'Archive/';
-	public const ATTACHMENT_DIR = 'Attachments/';
+class KeepJSONMarkdownConverter {
+    public const ARCHIVE_DIR = 'Archive/';
+    public const ATTACHMENT_DIR = 'Attachments/';
 
     /**
      * @var string
@@ -73,10 +72,10 @@ class KeepJSONMarkdownConverter
      * @var array
      */
     public array $attachments;
-	/**
-	 * @var string[]
-	 */
-	public array $attachmentsToCopy = [];
+    /**
+     * @var string[]
+     */
+    public array $attachmentsToCopy = [];
     /**
      * @var Document
      */
@@ -91,8 +90,7 @@ class KeepJSONMarkdownConverter
      *
      * @throws Exception
      */
-    public function __construct(stdClass $json_note)
-    {
+    public function __construct(stdClass $json_note) {
         $this->document = new Document();
         if (property_exists($json_note, 'color')) {
             $this->initColor($json_note->color);
@@ -139,8 +137,7 @@ class KeepJSONMarkdownConverter
      *
      * @return void
      */
-    private function initColor($color): void
-    {
+    private function initColor($color): void {
         if (is_string($color)) {
             $this->color = $color;
         }
@@ -151,8 +148,7 @@ class KeepJSONMarkdownConverter
      *
      * @return void
      */
-    private function initIsTrashed($isTrashed): void
-    {
+    private function initIsTrashed($isTrashed): void {
         if (is_bool($isTrashed)) {
             $this->isTrashed = $isTrashed;
         }
@@ -163,8 +159,7 @@ class KeepJSONMarkdownConverter
      *
      * @return void
      */
-    private function initIsPinned($isPinned): void
-    {
+    private function initIsPinned($isPinned): void {
         if (is_bool($isPinned)) {
             $this->isPinned = $isPinned;
         }
@@ -175,8 +170,7 @@ class KeepJSONMarkdownConverter
      *
      * @return void
      */
-    private function initIsArchived($isArchived): void
-    {
+    private function initIsArchived($isArchived): void {
         if (is_bool($isArchived)) {
             $this->isArchived = $isArchived;
         }
@@ -187,8 +181,7 @@ class KeepJSONMarkdownConverter
      *
      * @return void
      */
-    private function initTextContent($textContent): void
-    {
+    private function initTextContent($textContent): void {
         if (is_string($textContent)) {
             $this->textContent = $textContent;
         }
@@ -199,8 +192,7 @@ class KeepJSONMarkdownConverter
      *
      * @return void
      */
-    private function initListContent($listContent): void
-    {
+    private function initListContent($listContent): void {
         if (is_array($listContent)) {
             $this->listContent = $listContent;
         }
@@ -209,26 +201,27 @@ class KeepJSONMarkdownConverter
     /**
      * @throws Exception
      */
-    private function initTitle(\stdClass $json_note): void
-    {
-		$slugGenerator = new SlugGenerator((new SlugOptions())
-			->setDelimiter(' ')
-			->setValidChars('a-zA-Z0-9'));
+    private function initTitle(\stdClass $json_note): void {
+        $slugGenerator = new SlugGenerator((new SlugOptions())
+            ->setDelimiter(' ')
+            ->setValidChars('a-zA-Z0-9'));
         if (is_string($json_note->title) && $json_note->title) {
-			echo $json_note->title . "\r\n";
-			$this->title = $json_note->title;
-		} elseif (isset($json_note->annotations) && count($json_note->annotations) === 1 && $json_note->textContent === $json_note->annotations[0]->url) {
-			$this->title = $json_note->annotations[0]->title;
+            echo $json_note->title . "\r\n";
+            $this->title = $json_note->title;
+        } elseif (isset($json_note->annotations)
+            && count($json_note->annotations) === 1
+            && $json_note->textContent === $json_note->annotations[0]->url) {
+            $this->title = $json_note->annotations[0]->title;
         } elseif (isset($this->createdTime)) {
-			$this->title = $this->createdTime->format('Y-m-d-h-i-s');
-			$slugGenerator = new SlugGenerator((new SlugOptions())->setDelimiter('-'));
-		} else {
-			throw new \RuntimeException("No usable title can be derived from this note.");
-		}
+            $this->title = $this->createdTime->format('Y-m-d-h-i-s');
+            $slugGenerator = new SlugGenerator((new SlugOptions())->setDelimiter('-'));
+        } else {
+            throw new \RuntimeException("No usable title can be derived from this note.");
+        }
         $this->filename = $slugGenerator->generate($this->title, ['validChars' => 'A-Za-z0-9']) . '.md';
-		if ($this->isArchived) {
-			$this->filename = self::ARCHIVE_DIR . $this->filename;
-		}
+        if ($this->isArchived) {
+            $this->filename = self::ARCHIVE_DIR . $this->filename;
+        }
     }
 
     /**
@@ -236,11 +229,10 @@ class KeepJSONMarkdownConverter
      *
      * @return void
      */
-    private function initUserEditedTimestampUsec($userEditedTimestampUsec): void
-    {
+    private function initUserEditedTimestampUsec($userEditedTimestampUsec): void {
         if (is_int($userEditedTimestampUsec)) {
             // divide by one million because these are microseconds and unix time uses seconds
-            $unixTime = (int) ($userEditedTimestampUsec / 1000000);
+            $unixTime = (int)($userEditedTimestampUsec / 1000000);
             $this->modifiedTime = new DateTime("@$unixTime");
         }
     }
@@ -250,11 +242,10 @@ class KeepJSONMarkdownConverter
      *
      * @return void
      */
-    private function initCreatedTimestampUsec($createdTimestampUsec): void
-    {
+    private function initCreatedTimestampUsec($createdTimestampUsec): void {
         if (is_int($createdTimestampUsec)) {
             // divide by one million because these are microseconds and unix time uses seconds
-            $unixTime = (int) ($createdTimestampUsec / 1000000);
+            $unixTime = (int)($createdTimestampUsec / 1000000);
             $this->createdTime = new DateTime("@$unixTime");
         }
     }
@@ -264,8 +255,7 @@ class KeepJSONMarkdownConverter
      *
      * @return void
      */
-    private function initAnnotations($annotations): void
-    {
+    private function initAnnotations($annotations): void {
         if (is_array($annotations)) {
             $this->annotations = $annotations;
         }
@@ -276,12 +266,11 @@ class KeepJSONMarkdownConverter
      *
      * @return void
      */
-    private function initLabels($labels): void
-    {
+    private function initLabels($labels): void {
         if (is_array($labels)) {
-			$this->labels = array_map(static function ($label) {
-				return $label->name;
-			}, $labels);
+            $this->labels = array_map(static function ($label) {
+                return $label->name;
+            }, $labels);
         }
     }
 
@@ -290,8 +279,7 @@ class KeepJSONMarkdownConverter
      *
      * @return void
      */
-    private function initAttachments($attachments): void
-    {
+    private function initAttachments($attachments): void {
         if (is_array($attachments)) {
             $this->attachments = $attachments;
         }
@@ -300,8 +288,7 @@ class KeepJSONMarkdownConverter
     /**
      * @return void
      */
-    private function processDocumentPieces(): void
-    {
+    private function processDocumentPieces(): void {
         if (isset($this->title)) {
             $this->document->addElement(Element::createHeading($this->title, '1'));
             $this->document->addElement(Element::createBreak());
@@ -333,21 +320,21 @@ class KeepJSONMarkdownConverter
             $this->document->addElement(Element::createList($list));
         }
 
-		if (isset($this->attachments)) {
-			$new_lines = [];
-			foreach($this->attachments as $attachment) {
-				if (strpos($attachment->mimetype, 'image/') === 0) {
-					$basename = basename($attachment->filePath);
-					$dstFilename = ($this->isArchived ? '../' : '') . self::ATTACHMENT_DIR . $basename;
-					$new_lines[] = Element::createImage($dstFilename, $basename, $basename);
-					$this->attachmentsToCopy[] = $attachment->filePath;
-				} else {
-					// No support for recorded audio yet
-					throw new \RuntimeException('Unknown mimetype:' . $attachment->mimetype);
-				}
-			}
-			$this->document->addElement(Element::concatenateElements(...$new_lines));
-		}
+        if (isset($this->attachments)) {
+            $new_lines = [];
+            foreach ($this->attachments as $attachment) {
+                if (strpos($attachment->mimetype, 'image/') === 0) {
+                    $basename = basename($attachment->filePath);
+                    $dstFilename = ($this->isArchived ? '../' : '') . self::ATTACHMENT_DIR . $basename;
+                    $new_lines[] = Element::createImage($dstFilename, $basename, $basename);
+                    $this->attachmentsToCopy[] = $attachment->filePath;
+                } else {
+                    // No support for recorded audio yet
+                    throw new \RuntimeException('Unknown mimetype:' . $attachment->mimetype);
+                }
+            }
+            $this->document->addElement(Element::concatenateElements(...$new_lines));
+        }
 
         if (isset($this->annotations)) {
             foreach ($this->annotations as $annotation) {
@@ -363,44 +350,44 @@ class KeepJSONMarkdownConverter
             }
         }
 
-		if (!isset($this->labels)) {
-			$this->labels = ['nolabel'];
-		}
-		if (isset($this->color)) {
-			$this->labels[] = $this->color;
-		}
-		$tags = array_map(static function (string $label) {
-			return '#' . str_replace(' ', '', $label);
-		}, $this->labels);
-		if ($tags) {
-			$this->document->addElement(Element::createBreak());
-			$this->document->addElement(Element::createParagraph(implode(' ', $tags)));
-		}
+        if (!isset($this->labels)) {
+            $this->labels = ['nolabel'];
+        }
+        if (isset($this->color)) {
+            $this->labels[] = $this->color;
+        }
+        $tags = array_map(static function (string $label) {
+            return '#' . str_replace(' ', '', $label);
+        }, $this->labels);
+        if ($tags) {
+            $this->document->addElement(Element::createBreak());
+            $this->document->addElement(Element::createParagraph(implode(' ', $tags)));
+        }
     }
 
-	/**
-	 * @param string $basePath
-	 * @return string[][]
-	 */
-	public function getFilesToCopy(string $basePath): array {
-		$result = [];
-		foreach ($this->attachmentsToCopy as $srcFilename) {
-			$dstFilename = $srcFilename;
-			if (!file_exists($basePath . $srcFilename)) {
-				// bug in Google Takeout? Some attachments seem to have the wrong extension
-				$srcFilename = str_replace('.jpeg', '.jpg', $srcFilename);
-				if (!file_exists($basePath . $srcFilename)) {
-					echo $this->title . ': attachment not found: ' . $basePath .
-						$dstFilename . PHP_EOL;
-					continue;
-				}
-			}
-			$result[] = [
-				$srcFilename,
-				self::ATTACHMENT_DIR . basename($dstFilename),
-			];
-		}
+    /**
+     * @param string $basePath
+     * @return string[][]
+     */
+    public function getFilesToCopy(string $basePath): array {
+        $result = [];
+        foreach ($this->attachmentsToCopy as $srcFilename) {
+            $dstFilename = $srcFilename;
+            if (!file_exists($basePath . $srcFilename)) {
+                // bug in Google Takeout? Some attachments seem to have the wrong extension
+                $srcFilename = str_replace('.jpeg', '.jpg', $srcFilename);
+                if (!file_exists($basePath . $srcFilename)) {
+                    echo $this->title . ': attachment not found: ' . $basePath .
+                        $dstFilename . PHP_EOL;
+                    continue;
+                }
+            }
+            $result[] = [
+                $srcFilename,
+                self::ATTACHMENT_DIR . basename($dstFilename),
+            ];
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 }
